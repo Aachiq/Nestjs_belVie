ea96369b5c5cc9f675fd880aef06c8ab19a94270
Ecodexer <elhocine@ecodexia.com>
2026-08-12 17:27:16 +0100

    add first entity with loadAllEntities but uisng synchronize true & WIP move config outside

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,28 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // available everywhere
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      synchronize: true,
+
+      // entities: [Category, Product, User] or do "autoLoadENtities"
+      autoLoadEntities: true,
+    }),
+
     ProductsModule,
     CategoriesModule,
     UsersModule,
